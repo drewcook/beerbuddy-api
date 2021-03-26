bcc45c3e170b455bfff559316a842e379703c23d
Drew Cook <drewcook29@gmail.com>
2021-03-26 17:12:20 -0600

    remove pug templating, handle homepage

--- index.js
+++ index.js
@@ -22,12 +22,6 @@ if (process.env.NODE_ENV === 'production') {
   require('./startup/productionMiddleware')(app);
 }
 
-// **** THIS ISN'T NECESSARY FOR API BACKENDS ONLY, but...
-// Set the templating engine to the pug package
-app.set('view engine', 'pug');
-// this is default so doens't need to explicitly be set, but for demo purposes...
-app.set('views', './views');
-
 // Listen on server port for requests
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => logger.info(`Listening on port ${port}...`));
--- routes/homepage.js
+++ routes/homepage.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Pug templating
 router.get('/', (req, res) => {
-  // This will look at our pug views and serve up index.js using the provided variables
-  res.render('index', {
-    title: 'The BeerBuddy App',
-    message: 'BeerBuddy Service API'
-  });
+	res.status(200).send('Why are you here?');
 });
 
 module.exports = router;
