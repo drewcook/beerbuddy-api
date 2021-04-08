9856c86b149a9e928edc0cf83f258fb1e0e633e6
Drew Cook <drewcook29@gmail.com>
2021-04-07 21:17:27 -0600

    commen t

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,7 @@
 const config = require('config')
 
 const setupConfig = () => {
+	// TODO: use .env or generate an app secret and store it
 	if (!config.get('BEERBUDDY_APP_SECRET')) {
 		throw new Error('FATAL ERROR: BEERBUDDY_APP_SECRET is not defined.')
 	}
